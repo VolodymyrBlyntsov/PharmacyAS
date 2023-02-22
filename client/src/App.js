2b1e7a9a3e905ad494361657e43669a93c976c97
Volodymyr <volodymyr.blyntsov@gmail.com>
2023-02-22 15:26:46 +0200

    PharmacyAS
    * Created components and paths using React Route.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Drugs from './pages/Drugs';
+import Add from './pages/Add';
+import Update from './pages/Update';
 
 function App() {
   return (
     <div className="App">
-      Hi!
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Drugs />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/update" element={<Update />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
