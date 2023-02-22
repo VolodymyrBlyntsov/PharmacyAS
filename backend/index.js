be3bd65fd4b8d78504259d1f9f3cf40037ffc76e
Volodymyr <volodymyr.blyntsov@gmail.com>
2023-02-22 11:45:32 +0200

    PharmacyAS
    * Added database to server side.

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,12 @@
 import express from "express"
+import mysql from "mysql"
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "AS",
+    database: "pharmacy"
+})
 
 const app = express()
 
