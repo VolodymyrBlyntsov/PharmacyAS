9ef6ad5e7408e969641c60e78e94a6c668ecc643
Volodymyr <volodymyr.blyntsov@gmail.com>
2023-02-22 16:14:57 +0200

    PharmacyAS
    * Developed a mechanism for reading data from the server part and outputting data to the client part.

--- client/src/pages/Drugs.jsx
+++ client/src/pages/Drugs.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
 const Drugs = () => {
-  return (
-    <div>Drugs</div>
-  )
+    const [drugs, setDrugs] = useState([])
+
+    useEffect(() => {
+        const fetchAllDrugs = async () => {
+            try {
+                const res = await axios.get("http://localhost:8800/drugs")
+                setDrugs(res.data)
+                console.log(res)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchAllDrugs()
+    }, [])
+
+    return (
+        <div>
+            <h1>Pharmacy</h1>
+            <div className="drugs">
+                {drugs.map(drug => (
+                    <div className='drug' key={drug.id}>
+                        {drug.cover && <img src={drug.cover} alt="" />}
+                        <h2>{drug.title}</h2>
+                        <p>{drug.desc}</p>
+                        <span>{drug.price}</span>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default Drugs
