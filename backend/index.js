3ec9acd12f45b598baaead02fe9450ec5d1576fd
Volodymyr <volodymyr.blyntsov@gmail.com>
2023-02-22 12:54:28 +0200

    PharmacyAS
    ** Prescribed POST request, automated for client data input/output. Tested by Postman.
    
    ** Changed the port to the actual port: 8800.
    
    * Database access password has been changed. Now it is: root.
    
    * Added dynamically linking the client with the express server.
    
    * Minor changes in MySQL queries.

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,13 @@ import mysql from "mysql"
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "AS",
+    password: "root",
     database: "pharmacy"
 })
 
 const app = express()
+
+app.use(express.json())
 
 app.get("/", (req, res) => {
     res.json("Hello this is the backend!")
@@ -16,13 +18,26 @@ app.get("/", (req, res) => {
 
 app.get("/drugs", (req, res) => {
     const q = "SELECT * FROM drugs"
+    db.connect()
     db.query(q, (err, data) => {
         if (err) return res.json(err)
         return res.json(data)
     })
 })
 
-app.listen(8880, () => {
+app.post("/drugs", (req, res) => {
+    const q = "INSERT INTO drugs (`title`, `desc`, `cover`) VALUES (?)"
+    const values = [req.body.title,
+                    req.body.desc,
+                    req.body.cover]
+
+    db.query(q, [values], (err, data) => {
+        if (err) return res.json(err)
+        return res.json("Drug has been created successfully.")
+    })
+})
+
+app.listen(8800, () => {
     console.log("Connected to backend!")
 })
 
